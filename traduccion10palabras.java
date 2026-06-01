49d7fd8ad7c61c6c5933897a3c9e164553d2f0bf
Francisco G. Salinas <frangersal@outlook.com>
2026-06-01 16:26:38 -0600

    1.6 Ejercicios (Ejercicio 3)

---
+++ traduccion10palabras.java
@@ -0,0 +1,14 @@
+public class traduccion10palabras {
+        public static void main(String[] args) {
+        System.out.println("Hello         Hola");
+        System.out.println("Car           Carro");
+        System.out.println("Computer      Computadora");
+        System.out.println("Light         Luz");
+        System.out.println("Green         Verde");
+        System.out.println("Book          Libro");
+        System.out.println("Spider        Araña");
+        System.out.println("Cat           Gato");
+        System.out.println("Game          Juego");
+        System.out.println("Dog           Perro");
+    }
+}
