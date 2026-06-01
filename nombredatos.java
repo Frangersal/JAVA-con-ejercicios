ba6b9cd1ada9a486d62bdcd30f2ff17242a8df72
Francisco G. Salinas <frangersal@outlook.com>
2026-05-31 19:41:31 -0600

    1.6 Ejercicios (Ejercicio 2)

---
+++ nombredatos.java
@@ -0,0 +1,7 @@
+public class nombredatos {
+        public static void main(String[] args) {
+        System.out.println("Francisco Gerardo Salinas Mendoza");
+        System.out.println("Acapulco, Guerrero, México");
+        System.out.println("56 3746 2715");
+    }
+}
