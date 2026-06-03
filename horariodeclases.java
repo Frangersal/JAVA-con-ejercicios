d01d80104e148edb228ccb629e7609baadf896ed
Francisco G. Salinas <frangersal@outlook.com>
2026-06-02 23:47:19 -0600

    1.6 Ejercicios (Ejercicio 4)

---
+++ horariodeclases.java
@@ -0,0 +1,15 @@
+public class horariodeclases {
+    public static void main(String[] args) {
+        System.out.println("===== HORARIO DE CLASES =====");
+        System.out.println();
+        System.out.println("Hora         | Lunes        | Martes       | Miercoles    | Jueves       | Viernes");
+        System.out.println("-------------|--------------|--------------|--------------|--------------|--------------");
+        System.out.println("08:00-09:00  | Matematicas  | Lengua       | Matematicas  | Lengua       | Ingles");
+        System.out.println("09:00-10:00  | Fisica       | Historia     | Quimica      | Biologia     | Matematicas");
+        System.out.println("10:00-11:00  | Ingles       | Matematicas  | Ingles       | Fisica       | Historia");
+        System.out.println("11:00-11:30  | --- RECREO --- RECREO --- RECREO --- RECREO --- RECREO ---");
+        System.out.println("11:30-12:30  | Programacion | Programacion | Programacion | Programacion | Programacion");
+        System.out.println("12:30-13:30  | Educ. Fisica | Arte         | Educ. Fisica | Musica       | Filosofia");
+        System.out.println("13:30-14:30  | Tutoria      | Quimica      | Historia     | Geografia    | Laboratorio");
+    }
+}
