9998e48ccffaccb4e75302cec944e32fec88405e
Francisco G. Salinas <frangersal@outlook.com>
2026-06-12 21:28:28 -0600

    2.1 Definición y tipos de variables (Cadena de texto/String)

---
+++ UsoDeString.java
@@ -0,0 +1,9 @@
+public class UsoDeString {
+    public static void main(String[] args) {
+        String miPalabra = "cerveza";
+        String miFrase = "¿dónde está mi cerveza?";
+
+        System.out.println("Una palabra que uso confrecuencia: "+ miPalabra);
+        System.out.println("Una frase que uso a veces: "+ miFrase);
+    }
+}
