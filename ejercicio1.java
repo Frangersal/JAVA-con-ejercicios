1013a3c465cf127610b0eb3f571928f435e0efe9
Francisco G. Salinas <frangersal@outlook.com>
2026-06-19 23:19:18 -0600

    2.6 Ejercicios (Ejercicio 1)

---
+++ ejercicio1.java
@@ -0,0 +1,14 @@
+public class ejercicio1 {
+    public static void main(String[] args) {
+        int x = 144;
+        int y = 999;
+        int suma = x+y;
+        int resta = x-y;
+        double division = x/y;
+        double multiplicacion = x*y;
+        System.out.println("La suma de "+x+" y "+y+" es: "+suma);
+        System.out.println("La resta de "+x+" y "+y+" es: "+resta);
+        System.out.println("La division de "+x+" y "+y+" es: "+division);
+        System.out.println("La multiplicacion de "+x+" y "+y+" es: "+multiplicacion);
+    }
+}
