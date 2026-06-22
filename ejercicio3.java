5da46f736aaaed140424635f85606894de8e2c0d
Francisco G. Salinas <frangersal@outlook.com>
2026-06-21 21:46:28 -0600

    2.6 Ejercicios (Ejercicio 3)

---
+++ ejercicio3.java
@@ -0,0 +1,11 @@
+public class ejercicio3 {
+    public static void main(String[] args) {
+        String nombre = "Francisco";
+        String direccion = "Acapulco";
+        int numero = 1234567890;
+
+        System.out.println(nombre);
+        System.out.println(direccion);
+        System.out.println(numero);
+    }
+}
