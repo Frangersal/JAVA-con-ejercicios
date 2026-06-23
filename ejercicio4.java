dd4a9056804a946342c0b191825ffbfff3d46251
Francisco G. Salinas <frangersal@outlook.com>
2026-06-22 23:47:15 -0600

    2.6 Ejercicios (Ejercicio 4)

---
+++ ejercicio4.java
@@ -0,0 +1,9 @@
+public class ejercicio4 {
+    public static void main(String[] args) { 
+        double dolar = 17.41;
+        double peso = 300;
+        double conversion = peso/dolar;
+
+        System.out.println(peso +" pesos es igual a "+ conversion + "dolares"); 
+    }
+}
