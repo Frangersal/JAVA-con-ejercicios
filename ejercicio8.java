ec97330a2e554f61604780054edf2e958981a929
Francisco G. Salinas <frangersal@outlook.com>
2026-06-26 23:47:22 -0600

    2.6 Ejercicios (Ejercicio 8)

---
+++ ejercicio8.java
@@ -0,0 +1,13 @@
+public class ejercicio8 {
+    public static void main(String[] args) {
+        char letra1 = 'S';
+        char letra2 = 'a';
+        char letra3 = 'l';
+        char letra4 = 'u';
+        char letra5 = 'd';
+        
+        String palabra = "" + letra1 + letra2 + letra3 + letra4 + letra5;
+        
+        System.out.println(palabra);
+    }
+}
